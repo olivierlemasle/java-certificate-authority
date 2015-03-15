411a390904a23c852b06ddc32ce746bbbca5176e
olivierlemasle <olivier.lemasle@apalia.net>
2015-03-15 19:16:21 +0100

    Optimize CsrImpl

--- java-ca-lib/src/main/java/io/github/olivierlemasle/ca/CsrImpl.java
+++ java-ca-lib/src/main/java/io/github/olivierlemasle/ca/CsrImpl.java
@@ -7,24 +7,26 @@ import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
 import org.bouncycastle.pkcs.PKCS10CertificationRequest;
 
 class CsrImpl implements CSR {
-  private final PKCS10CertificationRequest request;
+  private final DistinguishedName dn;
+  private final PublicKey publicKey;
 
   public CsrImpl(final PKCS10CertificationRequest request) {
-    this.request = request;
+    dn = new BcX500NameDnImpl(request.getSubject());
+    try {
+      publicKey = (new JcaPEMKeyConverter()).getPublicKey(request.getSubjectPublicKeyInfo());
+    } catch (final PEMException e) {
+      throw new CaException(e);
+    }
   }
 
   @Override
   public DistinguishedName getSubject() {
-    return new BcX500NameDnImpl(request.getSubject());
+    return dn;
   }
 
   @Override
   public PublicKey getPublicKey() {
-    try {
-      return (new JcaPEMKeyConverter()).getPublicKey(request.getSubjectPublicKeyInfo());
-    } catch (final PEMException e) {
-      throw new CaException(e);
-    }
+    return publicKey;
   }
 
 }
