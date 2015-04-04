9929f798aef2470175d2344a89309527d0d39fda
olivierlemasle <olivier.lemasle@apalia.net>
2015-04-04 19:25:23 +0200

    Test https connection

--- java-ca-lib/src/test/java/io/github/olivierlemasle/tests/WindowsIT.java
+++ java-ca-lib/src/test/java/io/github/olivierlemasle/tests/WindowsIT.java
@@ -5,20 +5,28 @@ import io.github.olivierlemasle.ca.CSR;
 import io.github.olivierlemasle.ca.CertificateAuthority;
 import io.github.olivierlemasle.ca.DistinguishedName;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.lang.ProcessBuilder.Redirect;
-import java.net.URL;
-import java.net.URLConnection;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateEncodingException;
+import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.UUID;
 
+import javax.net.ssl.SSLContext;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
+import org.apache.http.util.EntityUtils;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -37,8 +45,8 @@ public class WindowsIT {
   }
 
   @Test
-  public void test() throws IOException, InterruptedException, CertificateEncodingException,
-      NoSuchAlgorithmException {
+  public void test() throws IOException, InterruptedException, NoSuchAlgorithmException,
+      KeyStoreException, KeyManagementException, CertificateException {
     // Create a CA
     final DistinguishedName caName = CA.dn("CN=CA-Test");
     final CertificateAuthority ca = CA.init().setName(caName).build();
@@ -46,29 +54,39 @@ public class WindowsIT {
     final X509Certificate caCert = ca.getCaCertificate();
     CA.export(caCert).saveCertificate("ca.cer");
 
+    // On Windows, install the CA certificate as a trusted certificate
+    // certutil -enterprise -addstore ROOT ca.cer
     installTrustedCert("ca.cer");
 
     // Generate CSR using Windows utilities
+    // certreq -new src\test\resources\csr_template.inf cert.req
     generateCsr();
 
+    // Load the generated CSR, sign it and export the resulting certificate
     final CSR csr = CA.loadCsr("cert.req").getCsr();
     final X509Certificate cert = ca.sign(csr);
     CA.export(cert).saveCertificate("cert.cer");
 
+    // On Windows, install the resulting certificate, with its private key
+    // certreq -accept cert.cer
     acceptCert("cert.cer");
+
+    // Configure SSL
     final String certThumbprint = getThumbPrint(cert);
+    // netsh http add sslcert ipport=0.0.0.0:443 certhash=... appid={...}
     configureSsl(certThumbprint, UUID.randomUUID().toString());
 
     // NB: https binding has been set in appveyor.yml
 
-    final URL url = new URL("https://localhost/");
-    final URLConnection connection = url.openConnection();
-    try (InputStream in = connection.getInputStream();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
-      String line = null;
-      while ((line = reader.readLine()) != null) {
-        System.out.println(line);
-      }
+    // Test the HTTPS connection
+    final KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+    keystore.load(null, null);
+    keystore.setCertificateEntry("cert", caCert);
+    final SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(keystore, null).build();
+    try (CloseableHttpClient httpClient = HttpClients.custom().setSslcontext(sslContext).build();
+        CloseableHttpResponse response = httpClient.execute(new HttpGet("https://localhost/"))) {
+      final HttpEntity entity = response.getEntity();
+      System.out.println(EntityUtils.toString(entity));
     }
 
   }
