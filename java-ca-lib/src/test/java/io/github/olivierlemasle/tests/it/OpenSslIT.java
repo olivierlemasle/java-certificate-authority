beb7408e60afcad2a37da33723742cc72a6e96a1
Olivier Lemasle <olivier.lemasle@apalia.net>
2016-03-11 00:00:56 +0100

    Update dependencies versions

--- java-ca-lib/src/test/java/io/github/olivierlemasle/tests/it/OpenSslIT.java
+++ java-ca-lib/src/test/java/io/github/olivierlemasle/tests/it/OpenSslIT.java
@@ -92,7 +92,7 @@ public class OpenSslIT {
     final SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(keystore, null).build();
     // Test the HTTPS connection
     System.out.println("Test https://localhost/");
-    try (CloseableHttpClient httpClient = HttpClients.custom().setSslcontext(sslContext).build();
+    try (CloseableHttpClient httpClient = HttpClients.custom().setSSLContext(sslContext).build();
         CloseableHttpResponse response = httpClient.execute(new HttpGet("https://localhost/"))) {
       final HttpEntity entity = response.getEntity();
       final String content = EntityUtils.toString(entity);
