5b5b5c9bd0b0c97c43df58a0f170f573aa1752af
Olivier Lemasle <olivier.lemasle@apalia.net>
2016-04-28 08:58:56 +0200

    Start styling with Bootstrap

--- ui/app/app.component.ts
+++ ui/app/app.component.ts
@@ -3,16 +3,28 @@ import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from "angular2/route
 import { HTTP_PROVIDERS } from "angular2/http";
 
 import { AuthoritiesComponent } from "./authorities.component";
+import { CertificatesComponent } from "./certificates.component";
 import { CaService } from "./ca.service";
 
 @Component({
   selector: "ca-app",
   template: `
-    <h1>{{title}}</h1>
-    <nav>
-      <a [routerLink]="['Authorities']">Authorities</a>
-    </nav>
-    <router-outlet></router-outlet>
+    <div class="component">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+          <h1>{{title}}</h1>
+          <ul class="nav nav-tabs">
+            <li class="nav-item">
+              <a [routerLink]="['Authorities']" class="nav-link">Authorities</a>
+            </li>
+            <li class="nav-item">
+              <a [routerLink]="['Certificates']" class="nav-link">Certificates</a>
+            </li>
+          </ul>
+          <router-outlet></router-outlet>
+        </div>
+      </div>
+    </div>
   `,
   directives: [ ROUTER_DIRECTIVES ],
   providers: [
@@ -27,6 +39,11 @@ import { CaService } from "./ca.service";
     name: "Authorities",
     component: AuthoritiesComponent,
     useAsDefault: true
+  },
+  {
+    path: "/certificates",
+    name: "Certificates",
+    component: CertificatesComponent
   }
 ])
 export class AppComponent {
---
+++ ui/app/certificates.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from "angular2/core";
+
+@Component({
+  selector: "certificates",
+  template: `
+    <p>TO DO</p>
+  `
+})
+export class CertificatesComponent { }
